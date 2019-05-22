662d92be9576dbc1487c27b7f0d25a3a98353254
Juan Manuel Benitez Carballo <manuel_carballo95@hotmail.com>
2019-05-21 21:33:55 -0400

    Commit Juan 21/05/2019

--- TPNomina/TPNomina/w_Registrar_Empleado.xaml.cs
+++ TPNomina/TPNomina/w_Registrar_Empleado.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,44 @@ namespace TPNomina
     /// </summary>
     public partial class w_Registrar_Empleado : Window
     {
+        ConexionBD datos;
         public w_Registrar_Empleado()
         {
             InitializeComponent();
+            datos = new ConexionBD();
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            //Instaciar el objeto de la clase Empleado
+            Empleado emp = new Empleado();
+
+            emp.Nombres = txtNombre.Text;
+            emp.Apellidos = txtApellido.Text;
+            emp.Nro_Documento = txtDocumento.Text;
+            emp.Direccion = txtDireccion.Text;
+            emp.Nro_Telefono = txtTelefono.Text;
+           // emp.Fecha_Nacimiento = dtpFechaNacimiento.SelectedDate;
+            emp.Fecha_Incorporacion = dtpFechaIncorporacion.SelectedDate;
+            emp.Imagen_Perfil = foto.Source.ToString();
+
+            //Guardamos los datos ingresados
+            datos.Empleado.Add(emp);
+            datos.SaveChanges();
+
+        }
+
+        private void BtnImagen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Elegir una imagen";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                foto.Source = new BitmapImage(new Uri(op.FileName));
+            }
         }
     }
 }

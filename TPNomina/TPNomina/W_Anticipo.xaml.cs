fd887f5f1823f74c136e860f7c3bea3864039530
meliruiz <ruizbogarin.meli@gmail.com>
2019-06-22 15:49:07 -0400

    commit Anticipo

--- TPNomina/TPNomina/W_Anticipo.xaml.cs
+++ TPNomina/TPNomina/W_Anticipo.xaml.cs
@@ -45,5 +45,44 @@ namespace TPNomina
         {
             CargarDatosGrilla();
         }
+
+        private void btnAprobar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgAnticipos.SelectedItem != null)
+            {
+                Anticipo A = (Anticipo)dgAnticipos.SelectedItem;
+
+
+                if (A.Estado.Equals("Pendiente"))
+                    A.Estado = "Aprobado";
+                else
+                    MessageBox.Show("La operacion no es valida");
+
+
+                Datos.Entry(A).State = System.Data.Entity.EntityState.Modified;
+                Datos.SaveChanges();
+                CargarDatosGrilla();
+                
+            }
+
+        }
+
+        private void btnRechazar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgAnticipos.SelectedItem != null)
+            {
+                Anticipo A = (Anticipo)dgAnticipos.SelectedItem;
+
+
+                if (A.Estado.Equals("Pendiente"))
+                    A.Estado = "Rechazado";
+                else
+                    MessageBox.Show("La operacion no es valida");
+
+
+                Datos.Entry(A).State = System.Data.Entity.EntityState.Modified;
+                Datos.SaveChanges();
+                CargarDatosGrilla();
+            }
     }
 }

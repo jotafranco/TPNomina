af135580db448efde38097b60905b00e3f0d56ad
Jota Franco <jfhado@gmail.com>
2019-07-10 18:28:20 -0400

    c

--- TPNomina/TPNomina/CrystalReport1.cs
+++ TPNomina/TPNomina/CrystalReport1.cs
@@ -119,6 +119,14 @@ namespace TPNomina {
                 return this.DataDefinition.ParameterFields[1];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_fecha {
+            get {
+                return this.DataDefinition.ParameterFields[2];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
--- TPNomina/TPNomina/W_Reporte.xaml.cs
+++ TPNomina/TPNomina/W_Reporte.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,5 +35,104 @@ namespace TPNomina
             CrystalReportViewer1.ViewerCore.ReportSource = CR;
 
         }
+
+        private void BtnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            //CrystalReport1 cryRpt = new CrystalReport1();
+
+            //cryRpt.Load(@"C:\Users\J\Source\Repos\TPNomina\TPNomina\TPNomina\CrystalReport1.rpt");
+
+
+
+            //ParameterFieldDefinitions crParameterFieldDefinitions;
+
+            //ParameterFieldDefinition crParameterFieldDefinition;
+
+            //ParameterValues crParameterValues = new ParameterValues();
+
+            //ParameterDiscreteValue crParameterDiscreteValue = new ParameterDiscreteValue();
+
+
+
+            //crParameterDiscreteValue.Value = txtMes.Text;
+
+            //crParameterFieldDefinitions = cryRpt.DataDefinition.ParameterFields;
+
+            ////crParameterFieldDefinition = crParameterFieldDefinitions["Mes"];
+            ////crParameterFieldDefinition = crParameterFieldDefinitions["Anho"];
+            //crParameterFieldDefinition = crParameterFieldDefinitions["Anho"];
+
+            //crParameterValues = crParameterFieldDefinition.CurrentValues;
+
+
+
+            //crParameterValues.Clear();
+
+            //crParameterValues.Add(crParameterDiscreteValue);
+
+            //crParameterFieldDefinition.ApplyCurrentValues(crParameterValues);
+
+            //cryRpt.Refresh();
+
+            //CrystalReportViewer1.ViewerCore.ReportSource = cryRpt;
+
+
+            ////CrystalReportViewer1.ViewerCore.RefreshReport(); 
+            ///
+
+
+
+
+            //ReportDocument cryRpt = new ReportDocument();
+            //cryRpt.Load(@"C:\Users\J\Source\Repos\TPNomina\TPNomina\TPNomina\CrystalReport1.rpt");
+
+            //TableLogOnInfos crtableLogoninfos = new TableLogOnInfos();
+            //TableLogOnInfo crtableLogoninfo = new TableLogOnInfo();
+            //ConnectionInfo crConnectionInfo = new ConnectionInfo();
+            //Tables CrTables;
+
+            //ParameterFieldDefinitions crParameterFieldDefinitions;
+            //ParameterFieldDefinition crParameterFieldDefinition;
+            //ParameterValues crParameterValues = new ParameterValues();
+            //ParameterDiscreteValue crParameterDiscreteValue = new ParameterDiscreteValue();
+
+            //crParameterDiscreteValue.Value = txtMes.Text;
+            //crParameterFieldDefinitions = cryRpt.DataDefinition.ParameterFields;
+            //crParameterFieldDefinition = crParameterFieldDefinitions["Mes"];
+            //crParameterValues = crParameterFieldDefinition.CurrentValues;
+
+            //crParameterValues.Clear();
+            //crParameterValues.Add(crParameterDiscreteValue);
+            //crParameterFieldDefinition.ApplyCurrentValues(crParameterValues);
+
+            //crParameterDiscreteValue.Value = txtAnho.Text;
+            //crParameterFieldDefinitions = cryRpt.DataDefinition.ParameterFields;
+            //crParameterFieldDefinition = crParameterFieldDefinitions["Anho"];
+            //crParameterValues = crParameterFieldDefinition.CurrentValues;
+
+            //crParameterValues.Add(crParameterDiscreteValue);
+            //crParameterFieldDefinition.ApplyCurrentValues(crParameterValues);
+
+            //crConnectionInfo.ServerName = @"LAPTOP-7FI8GS06\SQLEXPRESS";
+            //crConnectionInfo.DatabaseName = "Nomina";
+            //crConnectionInfo.UserID = "Sa";
+            //crConnectionInfo.Password = "j321";
+
+            //CrTables = cryRpt.Database.Tables;
+            //foreach (CrystalDecisions.CrystalReports.Engine.Table CrTable in CrTables)
+            //{
+            //    crtableLogoninfo = CrTable.LogOnInfo;
+            //    crtableLogoninfo.ConnectionInfo = crConnectionInfo;
+            //    CrTable.ApplyLogOnInfo(crtableLogoninfo);
+            //}
+
+            //CrystalReportViewer1.ViewerCore.ReportSource = cryRpt;
+            //CrystalReportViewer1.RefreshReport();
+            //cryRpt.Refresh();
+            //CrystalReportViewer1.();
+
+
+        }
+    
     }
 }
